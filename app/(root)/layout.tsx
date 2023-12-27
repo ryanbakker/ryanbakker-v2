3fff509420328c1a9eb57da8ce1c0e7765dd7e9d
ryanbakker <rmbakker2002@gmail.com>
2023-12-27 17:38:19 +1300

    Project Init

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,18 @@
+import Footer from "@/components/shared/Footer";
+import Header from "@/components/shared/Header";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex h-screen flex-col">
+      <main className="flex-1">
+        <Header />
+        {children}
+        <Footer />
+      </main>
+    </div>
+  );
+}
