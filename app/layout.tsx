6aba9369e3aa66b0b18a237f02f7f1458c483508
ryanbakker <rmbakker2002@gmail.com>
2024-02-28 11:23:29 +1300

    Security Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          footer: "hidden",
+        },
+      }}
+    >
       <html lang="en">
         <body className="bg-[#EEEEEE] dark:bg-[#1A1C29]">
           <ThemeProvider
