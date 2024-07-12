3499d0b81f7b0f12e19289a151ac823b88136857
ryanbakker <rmbakker2002@gmail.com>
2024-07-13 11:36:56 +1200

    Remove resume

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,7 +2,6 @@ import HeroSection from "@/components/shared/HeroSection";
 import ProjectSection from "@/components/shared/ProjectSection";
 import AboutSection from "@/components/shared/AboutSection";
 import ContactSection from "@/components/shared/ContactSection";
-import ResumeSection from "@/components/shared/ResumeSection";
 
 function Home() {
   return (
@@ -14,8 +13,6 @@ function Home() {
       <AboutSection />
 
       <ContactSection />
-
-      <ResumeSection />
     </>
   );
 }
--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,6 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   publicRoutes: [
     "/",
-    "/resume",
     "/projects",
     "/projects/:id",
     "/api/webhook/clerk",
