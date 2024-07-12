91ca784e136ab0ca9f3ccc9796cc46c0ff154c1e
ryanbakker <rmbakker2002@gmail.com>
2024-07-13 11:41:14 +1200

    Add GitHub section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,6 +2,7 @@ import HeroSection from "@/components/shared/HeroSection";
 import ProjectSection from "@/components/shared/ProjectSection";
 import AboutSection from "@/components/shared/AboutSection";
 import ContactSection from "@/components/shared/ContactSection";
+import ResumeSection from "@/components/shared/ResumeSection";
 
 function Home() {
   return (
@@ -13,6 +14,8 @@ function Home() {
       <AboutSection />
 
       <ContactSection />
+
+      <ResumeSection />
     </>
   );
 }
